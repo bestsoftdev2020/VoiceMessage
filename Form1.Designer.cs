8ebf344b3d65922e8c9212d1ee6d38837d0c056f
bestsoftdev2020 <bestsoftdev2020@yandex.ru>
2020-06-22 17:24:38 +0300

    First Published

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace VoiceMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VoiceMessage = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VoiceMessage
+            // 
+            this.VoiceMessage.ContextMenuStrip = this.contextMenuStrip1;
+            this.VoiceMessage.Icon = ((System.Drawing.Icon)(resources.GetObject("VoiceMessage.Icon")));
+            this.VoiceMessage.Text = "VoiceMessage";
+            this.VoiceMessage.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.OnMainTimer);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4000;
+            this.timer2.Tick += new System.EventHandler(this.OnStopRecording);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 67);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon VoiceMessage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Diagnostics;
+using NAudio.Wave;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+namespace VoiceMessage
+{
+    public partial class Form1 : Form
+    {
+        public WaveIn waveInStream ;
+        public WaveFileWriter writer;
+        public String timeStamp;
+        public String senderEmail;
+        public String receiverEmail;
+        public String fileSavePath = "AudioFileSaved/";
+        public Form1()
+        {
+            InitializeComponent();
+            System.IO.StreamReader file = new System.IO.StreamReader("config.txt");
+            string line;
+            string[] data;
+            int flag = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] spearator = { "<", ">" };
+
+                data = line.Split(spearator, 2, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string str in data)
+                {
+                    if (str == "interval")
+                        flag = 1;
+                    else if (str == "recording_time")
+                        flag = 2;
+                    else if (str == "sender_email")
+                        flag = 3;
+                    else if (str == "receiver_email")
+                        flag = 4;
+                    else if (str == "path")
+                        flag = 5;
+                    else
+                    {
+                        switch (flag)
+                        {
+                            case 1:
+                                timer1.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 2:
+                                timer2.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 3:
+                                senderEmail = str;
+                                break;
+                            case 4:
+                                receiverEmail = str;
+                                break;
+                            case 5:
+                                fileSavePath = str;
+                                break;
+                        }
+                    }
+                }
+            }
+            file.Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();       
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            this.Visible = false;
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            System.IO.StreamReader file = new System.IO.StreamReader("config.txt");
+            string line;
+            string[] data;
+            int flag = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] spearator = { "<", ">" };
+
+                data = line.Split(spearator, 2, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string str in data)
+                {
+                    if (str == "interval")
+                        flag = 1;
+                    else if (str == "recording_time")
+                        flag = 2;
+                    else if (str == "sender_email")
+                        flag = 3;
+                    else if (str == "receiver_email")
+                        flag = 4;
+                    else
+                    {
+                        switch (flag)
+                        {
+                            case 1:
+                                timer1.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 2:
+                                timer2.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 3:
+                                senderEmail = str;
+                                break;
+                            case 4:
+                                receiverEmail = str;
+                                break;
+                        }
+                    }
+                }
+            }
+            file.Close();
+            timer1.Enabled = true;
+        }
+
+        private void OnMainTimer(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer("notification.wav");
+            player.Play();
+            if (!Directory.Exists(@fileSavePath))
+                Directory.CreateDirectory(@fileSavePath);
+            timeStamp = @fileSavePath + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".wav";
+            waveInStream = new WaveIn();
+            writer = new WaveFileWriter(timeStamp, waveInStream.WaveFormat);
+            waveInStream.DataAvailable += new EventHandler<WaveInEventArgs>(waveInStream_DataAvailable);
+            waveInStream.StartRecording();
+            timer2.Enabled = true;
+        }
+
+        private void waveInStream_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+            int secondsRecorded = (int)(writer.Length / writer.WaveFormat.AverageBytesPerSecond);
+        }
+
+        private void OnStopRecording(object sender, EventArgs e)
+        {
+            waveInStream.StopRecording();
+            waveInStream.Dispose();
+            waveInStream = null;
+            writer.Close();
+            writer = null;
+            timer2.Enabled = false;
+  /*          SmtpClient smtpClient = new SmtpClient();
+            NetworkCredential smtpCredentials = new NetworkCredential("support@meuagente.com.br", "n(e{WIV%2?.g");
+
+            MailMessage message = new MailMessage();
+            MailAddress fromAddress = new MailAddress("support@meuagente.com.br");
+            MailAddress toAddress = new MailAddress("bedditmail@gmail.com");
+            MailAddress toAddress1 = new MailAddress("cechellafreelance@gmail.com");
+
+            smtpClient.Host = "mail.meuagente.com.br";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = false;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = smtpCredentials;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            message.From = fromAddress;
+            message.To.Add(toAddress);
+            message.To.Add(toAddress1);
+            message.IsBodyHtml = false;
+            message.Subject = "Voice Message";
+            message.Body = "A voice message was arrived!";
+            message.Attachments.Add(new Attachment(timeStamp));
+            smtpClient.Send(message); */
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            System.IO.StreamReader file = new System.IO.StreamReader("config.txt");
+            string line;
+            string[] data;
+            int flag = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] spearator = { "<", ">" };
+
+                data = line.Split(spearator, 2, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string str in data)
+                {
+                    if (str == "interval")
+                        flag = 1;
+                    else if (str == "recording_time")
+                        flag = 2;
+                    else if (str == "sender_email")
+                        flag = 3;
+                    else if (str == "receiver_email")
+                        flag = 4;
+                    else if (str == "path")
+                        flag = 5;
+                    else
+                    {
+                        switch (flag)
+                        {
+                            case 1:
+                                timer1.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 2:
+                                timer2.Interval = Int32.Parse(str) * 1000;
+                                break;
+                            case 3:
+                                senderEmail = str;
+                                break;
+                            case 4:
+                                receiverEmail = str;
+                                break;
+                            case 5:
+                                fileSavePath = str;
+                                break;
+                        }
+                    }
+                }
+            }
+            file.Close();
+            timer1.Enabled = true;
+        }
+    }
+}
